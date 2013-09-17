b81dfeaddb7fd967361a068d62069787170866f8
Rodrigo De la Garza <josille@gmail.com>
2013-09-17 12:02:15 +0200

    New authors description
    The new authors xml descriptions was done according to last
    communication with informindia technicians

--- jgate/JGateExportDom.inc.php
+++ jgate/JGateExportDom.inc.php
@@ -134,27 +134,9 @@ class JGateExportDom {
 		XMLCustomWriter::createChildWithText($doc, $articleNode, 'Pubdate', date('d-m-Y', $parsedPubdate));
 
 		/* Authors */
-		foreach ($article->getAuthors() as $author) {
-			
-			XMLCustomWriter::createChildWithText($doc, $articleNode, 'AuthorName', $author->getFullName());
-			/* Affiliation */
-			$affiliations = $author->getAffiliation(null);
-				
-			if (is_array($affiliations)) foreach ($affiliations as $locale => $affiliation) {
-
-				$curr_affis = explode("\\", $affiliation);
-				$curr_affis = array_filter($curr_affis);
-				$curr_affis = array_values($curr_affis);
-
-				foreach ($curr_affis as $curr_aff) {
-					XMLCustomWriter::createChildWithText($doc, $articleNode, 'AuthorAffiliation', trim($curr_aff));
-				}
-			}
-
-			/* email  */
-			XMLCustomWriter::createChildWithText($doc, $articleNode, 'AuthorEmails', $author->getEmail());
-		}
-
+		$AuthorsAffiNode =& JGateExportDom::generateAuthorsDom(&$doc, $articleNode, $article->getAuthors());
+		XMLCustomWriter::appendChild($articleNode, $AuthorsAffiNode);
+		
 
 		/* publisher_item is the article pages */
 		if ($article->getPages() != '') {
@@ -209,6 +191,200 @@ class JGateExportDom {
 		XMLCustomWriter::createChildWithText($doc, $publicationDateNode, 'year', date('Y', $parsedPubdate));
 
 		return $publicationDateNode;
+	}
+	
+	/**
+	 * Generate Authors
+	 * @param $doc XMLNode
+	 * @param $authors object
+	 * @return XMLNode
+	 */
+	function &generateAuthorsDom(&$doc, $articleNode, $authors) {
+		$authors_obj = $authors;
+		$authors_arr = array();
+		//$corrFlag = false;
+		$arr_affi = array();
+		/*
+		 * firts do authors
+		 */
+		foreach ($authors_obj as $index => $author)
+		{
+			$author_Salut = $author->getSalutation();
+			$author_Fname = $author->getFirstName();
+			$author_Lname = $author->getLastName();
+			$author_id = $author->getAuthorID();
+			
+			$author_indexes_arr = array();
+			
+			$one_affi = false;
+		
+			$affiliations = $author->getAffiliation(null);
+			
+			if (is_array($affiliations)) foreach ($affiliations as $locale => $affiliation) {
+				
+				$curr_affis = explode("\\", $affiliation);				
+				$curr_affis = array_filter($curr_affis);
+				$curr_affis = array_values($curr_affis);
+				
+				/*
+				 * Only one affiliation
+				 */
+				if(count($curr_affis) == 1)
+				{
+					$one_affi = true;
+					// one affiliation in the field
+					/*
+					 * If affiliation exist, get index to be placed to @author jos
+					 * if not insert in array and get index
+					 */
+					if(count($arr_affi)==0)
+					{
+						$arr_affi[] = trim($affiliation);
+						$author_indexes_arr[]='1';
+					}
+					else {
+						foreach ($curr_affis as $curr_aff) {
+														
+							/*
+							 * look if affiliation already exist
+							 */
+							$key = array_search(trim($curr_aff), $arr_affi);
+							if($key === false){
+							    $arr_affi[] = trim($curr_aff);
+							    $author_indexes_arr[] = count($arr_affi);
+							}
+							else {
+								 $author_indexes_arr[] = ($key+1);
+							}							
+						}
+					}
+				}
+				else {
+					/*
+					 * more than one affiliation
+					 */
+					
+					/*
+					 * If affiliation exist, get index to be placed to @author jos
+					 * if not insert in array and get index
+					 */
+					$key_index = 0;
+					if(count($arr_affi)==0)
+					{
+						foreach ($curr_affis as $key => $curr_aff) {
+							$arr_affi[] = trim($curr_aff);
+							
+							$author_indexes_arr[] = ($key+1);
+						}
+					}
+					else
+					{
+						foreach ($curr_affis as $key => $curr_aff) {
+													
+							$key = array_search(trim($curr_aff), $arr_affi);
+
+							if($key === false){
+							    $arr_affi[] = trim($curr_aff);
+							    $author_indexes_arr[] = count($arr_affi);
+							}
+							else {
+								 $author_indexes_arr[] = ($key+1);
+							}							
+						}
+					}
+				}
+			}
+			
+			//$author_text .= $author_name.'<sup>'.$author_indexes.'</sup>';
+			$single_author = false;
+		
+			if(count($authors_obj) == 1 && count($author_indexes_arr) == 1)
+			{
+				$single_author=true;
+			}
+		
+			if(count($authors_obj) >1 && $author->getPrimaryContact())
+			{
+				$corrFlag = true;
+				$authors_arr[] = array('id'=>$author_id,'user'=>$author,'salut'=> $author_Salut, 'fname' => $author_Fname,'lname' => $author_Lname,'ref'=>$author_indexes_arr,'corr'=>'*');
+			}
+			else
+			{
+				/*
+				 * Single author, if only one affi setr flag to not display affiliation number
+				 */
+				$authors_arr[] = array('id'=>$author_id,'user'=>$author,'salut'=> $author_Salut, 'fname' => $author_Fname,'lname' => $author_Lname,'ref'=>$author_indexes_arr);
+			}
+		}
+		
+		$authorGroupNode =& XMLCustomWriter::createElement($doc, 'AuthorGroup');
+		if(count($authors_arr)>0)
+		{
+			foreach ($authors_arr as $author) {
+				$user = $author['user'];
+				$referenceIndex = $author['ref'];
+				/*
+				 *   <Author AffiliationIDS="Aff1" CorrespondingAffiliationID="Aff1">
+                        <AuthorName DisplayOrder="Western">
+                           <Prefix>PD Dr.</Prefix>
+                           <GivenName>T.</GivenName>
+                           <FamilyName>Kriebel</FamilyName>
+                        </AuthorName>
+                        <Contact>
+                           <Email>tkriebe@gwdg.de</Email>
+                        </Contact>
+                     </Author>
+				 */
+				$authorNode =& XMLCustomWriter::createElement($doc, 'Author');
+				
+				/*
+				 * Make author references
+				 */
+				if(count($referenceIndex)>0)
+				{
+					$reftext='';
+					foreach ($referenceIndex as $key => $value) {
+						if($key != 0)
+						{
+							$reftext.=' ';
+						}
+						$reftext .= 'Aff'.$value;
+					}
+					XMLCustomWriter::setAttribute($authorNode, 'AffiliationIDS', $reftext);
+				}
+				
+				$authorNameNode =& XMLCustomWriter::createElement($doc, 'AuthorName');
+				
+				//XMLCustomWriter::createChildWithText($doc, $authorNameNode, 'Prefix', $author['salut']);
+				XMLCustomWriter::createChildWithText($doc, $authorNameNode, 'GivenName', $author['fname']);
+				XMLCustomWriter::createChildWithText($doc, $authorNameNode, 'FamilyName', $author['lname']);
+				
+				XMLCustomWriter::appendChild($authorNode, $authorNameNode);
+				
+				$contactNode =& XMLCustomWriter::createElement($doc, 'Contact');
+				XMLCustomWriter::createChildWithText($doc, $contactNode, 'Email', $user->getEmail());
+				
+				XMLCustomWriter::appendChild($authorNode, $contactNode);
+				
+				XMLCustomWriter::appendChild($authorGroupNode, $authorNode);
+			}
+			
+			/*
+				 * set affiliations and corresponding affiliation
+				 */
+				//if(isset($author['corr']))
+				//{
+				//	XMLCustomWriter::setAttribute($authorNode, 'CorrespondingAffiliationID', JGATE_XMLNS_XSI);
+				//}			
+				foreach ($arr_affi as $key => $affi) {
+					$affiliationNode =& XMLCustomWriter::createElement($doc, 'Affiliation');
+					XMLCustomWriter::setAttribute($affiliationNode, 'ID', 'Aff'.($key+1));
+					XMLCustomWriter::createChildWithText($doc, $affiliationNode, 'OrgName', $affi);
+					XMLCustomWriter::appendChild($authorGroupNode, $affiliationNode);
+				}
+		}
+		
+		return $authorGroupNode;
 	}
 }
 
